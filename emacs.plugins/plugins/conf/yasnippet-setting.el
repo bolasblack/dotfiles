50eb78ffc07ee1dc687c14327ab9cc2cc461af94
bolasblack <bolasblack@gmail.com>
2010-10-05 09:30:30 +0800

    deleted ‘emacs.plugins/plugins/zencoding-mode.el’ and 752 more

--- emacs.plugins/plugins/conf/yasnippet-setting.el
+++
@@ -1,9 +0,0 @@
-;; YAsnippet 的相关设置
-
-(add-to-list 'load-path "~/.emacs.d/plugins/yasnippet")
-(require 'yasnippet)
-(yas/initialize)
-(yas/load-directory "~/.emacs.d/plugins/yasnippet/snippets")
-
-(provide 'yasnippet-setting)
-
