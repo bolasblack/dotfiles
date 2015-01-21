61cc1ecf9f24b8ac07b57d7107d587b7ca270dcb
bolasblack <bolasblack@gmail.com>
2015-01-21 11:22:29 +0800

    feat(zsh): 允许自定义 $plugins 数组

--- zshfiles/antigen.zsh
+++ zshfiles/antigen.zsh
@@ -1,4 +1,3 @@
-plugins=(colored-man zsh-users/zsh-syntax-highlighting)
 
 PLUGIND=$HOME/.antigen/repos
 ANTIGEND=$PLUGIND/antigen
@@ -7,6 +6,7 @@ ANTIGEND=$PLUGIND/antigen
 [ -d $ANTIGEND ] || git clone --depth=1 https://github.com/zsh-users/antigen.git $ANTIGEND
 source $ANTIGEND/antigen.zsh
 
+[ -z "$plugins" ] && plugins=(colored-man zsh-users/zsh-syntax-highlighting)
 for plugin in $plugins; do
   antigen bundle $plugin
 done
