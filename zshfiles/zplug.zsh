db215eba20bf4f9a58da3afbdd40a21ccb0a0db3
c4605 <bolasblack@gmail.com>
2016-09-24 17:37:52 +0800

    change(zsh): 使用 zplug 替代 antigen ，不再把 / 视为单词的一部分

---
+++ zshfiles/zplug.zsh
@@ -0,0 +1,24 @@
+# https://github.com/zplug/zplug
+
+export ZPLUG_HOME=/usr/local/opt/zplug
+source $ZPLUG_HOME/init.zsh
+
+zplug "plugins/git", from:oh-my-zsh, if:"(( $+commands[git] ))"
+zplug "plugins/colored-man-pages", from:oh-my-zsh
+zplug "zsh-users/zsh-syntax-highlighting"
+zplug "k4rthik/git-cal", as:command, frozen:1
+zplug "b4b4r07/enhancd", use:init.sh
+zplug "bric3/nice-exit-code"
+
+export ENHANCD_FILTER="/usr/local/bin/fzf"
+
+# Install plugins if there are plugins that have not been installed
+if ! zplug check --verbose; then
+  printf "Install? [y/N]: "
+  if read -q; then
+    echo; zplug install
+  fi
+fi
+
+# Then, source plugins and add commands to $PATH
+zplug load --verbose
