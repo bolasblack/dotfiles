7a297d2c9460d8d17fd0e449e62ebb742f501ca5
c4605 <bolasblack@gmail.com>
2018-10-08 14:24:40 +0800

    scripts: backup-firefox-profiles.sh，移除 Firefox 目录，转而备份整个 Firefox Profiles

---
+++ scripts/backup-firefox-profiles.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+FirefoxPath="$HOME/Library/Application Support/Firefox"
+operateId=`mktemp -u XXXXXXXXXX`
+tempFile="`date -Idate`.$operateId.tbz2"
+
+echo "========================================="
+echo "Progress start"
+echo "========================================="
+
+osascript -e 'display notification "Started" with title "Firefox profiles backup"'
+
+echo "========================================="
+echo "Start archive files"
+echo "========================================="
+
+tar -cjvf "/tmp/backup-firefox-profiles-$tempFile" -C "$FirefoxPath" profiles.ini Profiles 2>&1
+
+echo "========================================="
+echo "Start encrypt file"
+echo "========================================="
+
+gpg --batch --encrypt -r 'bolasblack@gmail.com' -o ~/Dropbox/AppSettings/Firefox/$tempFile.gpg /tmp/backup-firefox-profiles-$tempFile
+
+if [ $? -eq 0 ]; then
+  osascript -e 'display notification "Succeed" with title "Firefox profiles backup"'
+else
+  osascript -e 'display notification "Failed" with title "Firefox profiles backup"'
+fi
+
+echo "========================================="
+echo "All finished"
+echo "========================================="
